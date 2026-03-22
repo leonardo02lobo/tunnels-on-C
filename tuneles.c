3430da03fe37ab6d3ee08501cb353b69bb5366c3
Leonardo Lobo <leonardo.lobo@unet.edu.ve>
2026-03-22 09:43:32 -0400

    Modificate structure

--- tuneles.c
+++ tuneles.c
@@ -15,120 +15,127 @@
 
 #define SIZE 256
 
-void to_uppercase(char *str) {
-	int i = 0;
-    for (i = 0; str[i]; i++) {
+void to_uppercase(char *str)
+{
+    int i = 0;
+    for (i = 0; str[i]; i++)
+    {
         str[i] = toupper(str[i]);
     }
 }
 
-int main(int argc, char *argv[]){
-    #ifdef _WIN32
-	    HANDLE hPipeRead, hPipeWrite;
-	    SECURITY_ATTRIBUTES sa = { sizeof(SECURITY_ATTRIBUTES), NULL, TRUE };
-	
-	    CreatePipe(&hPipeRead, &hPipeWrite, &sa, 0);
-	
-	    HANDLE hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, SIZE, "Global\\SharedMemory");
-	    char *shared_mem = (char *)MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, SIZE);
-	
-	    HANDLE sem = CreateSemaphore(NULL, 0, 1, "Global\\Semaphore");
+int main(int argc, char *argv[])
+{
+#ifdef _WIN32
+    HANDLE hPipeRead, hPipeWrite;
+    SECURITY_ATTRIBUTES sa = {sizeof(SECURITY_ATTRIBUTES), NULL, TRUE};
 
-	    if (argc > 1 && strcmp(argv[1], "hijo1") == 0) {
-	
-	        char buffer[SIZE];
-	
-	        printf("Ingrese texto: ");
-	        fgets(buffer, SIZE, stdin);
-	
-	        DWORD written;
-	        WriteFile(hPipeWrite, buffer, strlen(buffer) + 1, &written, NULL);
-	
-	        WaitForSingleObject(sem, INFINITE);
-	
-	        printf("Hijo1 recibe: %s\n", shared_mem);
-	        return 0;
-	    }
-	    if (argc > 1 && strcmp(argv[1], "hijo2") == 0) {
-	
-	        char buffer[SIZE];
-	        DWORD read;
-	
-	        ReadFile(hPipeRead, buffer, SIZE, &read, NULL);
-	
-	        to_uppercase(buffer);
-	
-	        strcpy(shared_mem, buffer);
-	
-	        ReleaseSemaphore(sem, 1, NULL);
-	        return 0;
-	    }
-	    STARTUPINFO si1 = { sizeof(si1) };
-	    PROCESS_INFORMATION pi1;
-	
-	    STARTUPINFO si2 = { sizeof(si2) };
-	    PROCESS_INFORMATION pi2;
-	
-	    char cmd1[] = "tuneles.exe hijo1";
-	    char cmd2[] = "tuneles.exe hijo2";
-	
-	    CreateProcess(NULL, cmd1, NULL, NULL, TRUE, 0, NULL, NULL, &si1, &pi1);
-	    CreateProcess(NULL, cmd2, NULL, NULL, TRUE, 0, NULL, NULL, &si2, &pi2);
-	
-	    WaitForSingleObject(sem, INFINITE);
-	
-	    printf("Padre lee: %s\n", shared_mem);
-	
-	    WaitForSingleObject(pi1.hProcess, INFINITE);
-	    WaitForSingleObject(pi2.hProcess, INFINITE);
-    #else
-        int pipefd[2];
-        pipe(pipefd);
+    CreatePipe(&hPipeRead, &hPipeWrite, &sa, 0);
 
-        char *shared_mem = mmap(NULL, SIZE, PROT_READ | PROT_WRITE,
-                                MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    HANDLE hMapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, SIZE, "Global\\SharedMemory");
+    char *shared_mem = (char *)MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, SIZE);
 
-        sem_t *sem = mmap(NULL, sizeof(sem_t),
-                        PROT_READ | PROT_WRITE,
-                        MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    HANDLE sem = CreateSemaphore(NULL, 0, 1, "Global\\Semaphore");
 
-        sem_init(sem, 1, 0);
+    if (argc > 1 && strcmp(argv[1], "hijo1") == 0)
+    {
 
-        pid_t pid1 = fork();
+        char buffer[SIZE];
 
-        if (pid1 == 0){
-            char buffer[SIZE];
+        printf("Ingrese texto: ");
+        fgets(buffer, SIZE, stdin);
 
-            printf("Ingrese texto: ");
-            fgets(buffer, SIZE, stdin);
+        DWORD written;
+        WriteFile(hPipeWrite, buffer, strlen(buffer) + 1, &written, NULL);
 
-            write(pipefd[1], buffer, strlen(buffer) + 1);
+        WaitForSingleObject(sem, INFINITE);
 
-            sem_wait(sem);
+        printf("Hijo1 recibe: %s\n", shared_mem);
+        return 0;
+    }
+    if (argc > 1 && strcmp(argv[1], "hijo2") == 0)
+    {
 
-            printf("Hijo1 recibe: %s\n", shared_mem);
-            exit(0);
-        }
+        char buffer[SIZE];
+        DWORD read;
 
-        pid_t pid2 = fork();
+        ReadFile(hPipeRead, buffer, SIZE, &read, NULL);
 
-        if (pid2 == 0){
-            char buffer[SIZE];
+        to_uppercase(buffer);
 
-            read(pipefd[0], buffer, SIZE);
+        strcpy(shared_mem, buffer);
 
-            to_uppercase(buffer);
+        ReleaseSemaphore(sem, 1, NULL);
+        return 0;
+    }
+    STARTUPINFO si1 = {sizeof(si1)};
+    PROCESS_INFORMATION pi1;
 
-            strcpy(shared_mem, buffer);
+    STARTUPINFO si2 = {sizeof(si2)};
+    PROCESS_INFORMATION pi2;
 
-            sem_post(sem);
-            exit(0);
-        }
+    char cmd1[] = "tuneles.exe hijo1";
+    char cmd2[] = "tuneles.exe hijo2";
 
-        wait(NULL);
-        wait(NULL);
+    CreateProcess(NULL, cmd1, NULL, NULL, TRUE, 0, NULL, NULL, &si1, &pi1);
+    CreateProcess(NULL, cmd2, NULL, NULL, TRUE, 0, NULL, NULL, &si2, &pi2);
 
-        printf("Padre lee: %s\n", shared_mem);
-    #endif
+    WaitForSingleObject(sem, INFINITE);
+
+    printf("Padre lee: %s\n", shared_mem);
+
+    WaitForSingleObject(pi1.hProcess, INFINITE);
+    WaitForSingleObject(pi2.hProcess, INFINITE);
+#else
+    int pipefd[2];
+    pipe(pipefd);
+
+    char *shared_mem = mmap(NULL, SIZE, PROT_READ | PROT_WRITE,
+                            MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    sem_t *sem = mmap(NULL, sizeof(sem_t),
+                      PROT_READ | PROT_WRITE,
+                      MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    sem_init(sem, 1, 0);
+
+    pid_t pid1 = fork();
+
+    if (pid1 == 0)
+    {
+        char buffer[SIZE];
+
+        printf("Ingrese texto: ");
+        fgets(buffer, SIZE, stdin);
+
+        write(pipefd[1], buffer, strlen(buffer) + 1);
+
+        sem_wait(sem);
+
+        printf("Hijo1 recibe: %s\n", shared_mem);
+        exit(0);
+    }
+
+    pid_t pid2 = fork();
+
+    if (pid2 == 0)
+    {
+        char buffer[SIZE];
+
+        read(pipefd[0], buffer, SIZE);
+
+        to_uppercase(buffer);
+
+        strcpy(shared_mem, buffer);
+
+        sem_post(sem);
+        exit(0);
+    }
+
+    wait(NULL);
+    wait(NULL);
+
+    printf("Padre lee: %s\n", shared_mem);
+#endif
     return 0;
 }
